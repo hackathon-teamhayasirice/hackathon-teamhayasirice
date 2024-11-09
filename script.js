03108466c00114dfe5f49aaceef0e0718df3d015
Tomu <ttomfjkw@gmail.com>
2024-11-10 02:32:07 +0900

    お部屋合体完了

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ↓使用していないとエラーになるのでコメントアウト
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
 // 選べる4つのコース
 $(document).ready(function () {
@@ -14,7 +14,51 @@ $(document).ready(function () {
 });
 // お食事
 
+
+
 // お部屋
+// 共通のトリガー要素を設定
+const triggerElement = ".two-card-container";
+
+// 中央位置を計算
+const centerX = window.innerWidth / 2;
+const imgTextContainer = document.querySelector(".room-image-text-container1");
+const imgTextContainer1Width = imgTextContainer.offsetWidth;
+// room-image-text-container1を中央から左に出現させる
+gsap.fromTo(
+  ".room-image-text-container1",
+  {
+    x: imgTextContainer1Width / 2 + 60,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+      trigger: triggerElement,
+    },
+  }
+);
+
+// room-image-text-container2を中央から右に出現させる
+gsap.fromTo(
+  ".room-image-text-container2",
+  {
+    x: -(imgTextContainer1Width / 2 + 60),
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+      trigger: triggerElement,
+    },
+  }
+);
+
+
 
 // アクセス
 window.onload = () =>{
