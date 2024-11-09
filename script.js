5151e133ccd305fc1f84badbd9476a8faacce190
Tomu <ttomfjkw@gmail.com>
2024-11-09 13:04:50 +0900

    top-access

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-gsap.registerPlugin(ScrollTrigger);
+// ↓使用していないとエラーになるのでコメントアウト
+// gsap.registerPlugin(ScrollTrigger);
 
-window.addEventListener("load", function () {
-  const introductionSection = document.querySelector(".introduction");
+// 選べる4つのコース
 
-  introductionSection.style.transform = "translateX(0)";
-});
-//　gsap文法　gsap.やりたいメゾット(".クラス名", {パラメータ}) どんどん下に追加していく
+// お食事
 
-gsap.set(".introduction", {
-  width: items.length * 100 + "%",
-}); //items.lengthはjs-itemの要素の数のこと．今回は3つあるのでwidthは300%になる．
+// お部屋
+
+// アクセス
+window.onload = () =>{
+  console.log("loaded");
+  let map = L.map('map').setView([34.635635, 132.62966], 16);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  let customIcon = L.icon({
+      iconUrl: './img/access-pin.png', // カスタムアイコンのURL
+      iconSize: [32, 32],                 // アイコンサイズ [幅, 高さ]
+      iconAnchor: [16, 32],               // アイコンの位置調整 [x, y]
+      popupAnchor: [0, -32]               // ポップアップの位置調整
+  });
+  // 目標地点にカスタムマーカーを配置
+let marker = L.marker([34.635635, 132.62966], { icon: customIcon }).addTo(map);
+
+// マーカーにポップアップを追加
+marker.bindPopup("HAYASHI GRAMPING").openPopup();
+}
