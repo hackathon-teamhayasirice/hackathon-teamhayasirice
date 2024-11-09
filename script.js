3abe314886ba7b4e2ba96cda40e1f9e924e9e807
Tomu <ttomfjkw@gmail.com>
2024-11-10 02:36:18 +0900

    お食事合体完了

--- script.js
+++ script.js
@@ -13,6 +13,42 @@ $(document).ready(function () {
   });
 });
 // お食事
+// 共通のトリガー要素を設定
+const triggerElement1 = ".meal-page-dev";
+
+// meal-image-text-container1を左からスライドイン
+gsap.fromTo(
+  ".meal-image-text-container1",
+  {
+    x: -window.innerWidth, // 画面の左端からスタート
+    opacity: 0,
+  },
+  {
+    x: 0, // 元の位置に移動
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+      trigger: triggerElement1,
+    },
+  }
+);
+
+// meal-image-text-container2を右からスライドイン
+gsap.fromTo(
+  ".meal-image-text-container2",
+  {
+    x: window.innerWidth, // 画面の右端からスタート
+    opacity: 0, // 初期状態は透明
+  },
+  {
+    x: 0, // 元の位置に移動
+    opacity: 1, // 不透明に
+    duration: 2,
+    scrollTrigger: {
+      trigger: triggerElement1,
+    },
+  }
+);
 
 
 
