a58e7af97e7cfd0b8a4b3ab9af534ef360fc9e57
Tomu <ttomfjkw@gmail.com>
2024-11-10 02:54:59 +0900

    周辺観光合体完了

--- script.js
+++ script.js
@@ -116,6 +116,23 @@ let marker = L.marker([34.635635, 132.62966], { icon: customIcon }).addTo(map);
 marker.bindPopup("HAYASHI GRAMPING").openPopup();
 }
 
+// 周辺観光
+$(document).ready(function () {
+  $(".autoplay").slick({
+    slidesToShow: 3, // 1回に表示するスライド数
+    slidesToScroll: 1, // 1回にスクロールするスライド数
+    autoplay: true, // 自動再生
+    autoplaySpeed: 1000, // 自動再生のスピード
+    infinite: true, // 無限ループ
+    centerMode: true, // 中央配置を有効にす
+    variableWidth: true, // スライドの幅を可変にする
+    dots: true, // ドットナビゲーション
+    focusOnSelect: true, // スライドをクリックで選択
+    gap: 100, // スライド間のスペースを調整するオプション
+  });
+});
+
+
 
 // カレンダー
 document.addEventListener('DOMContentLoaded', function() {
